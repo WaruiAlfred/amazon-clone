845285eb55b35da4d76942d240249a635238ad3d
WaruiAlfred <alfredwarui17@gmail.com>
2022-04-05 01:39:06 +0300

    create route pages folder

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Header from "../components/Header";
+
+function HomePage() {
+  return (
+    <Fragment>
+      <Header />
+      <h1>Home</h1>
+    </Fragment>
+  );
+}
+
+export default HomePage;
