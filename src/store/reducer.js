7ef94b294fc05ea5a3f2a99c73cf2fb93fe7e0d7
WaruiAlfred <alfredwarui17@gmail.com>
2022-04-06 02:05:21 +0300

    add set user action to reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,14 +3,32 @@ export const initialState = {
   user: null,
 };
 
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
   console.log(action);
   switch (action.type) {
+    case "SET_USER":
+      return { ...state, user: action.user };
     case "ADD_TO_BASKET":
       return { ...state, basket: [...state.basket, action.item] };
 
     case "REMOVE_FROM_BASKET":
-      return state;
+      let newBasket = [...state.basket];
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove item with id ${action.id} as it doesn't exist`
+        );
+      }
+
+      return { ...state, basket: newBasket };
 
     default:
       return state;
