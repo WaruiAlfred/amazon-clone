17f0586919f4b35ffeb374908c836931b474e349
WaruiAlfred <alfredwarui17@gmail.com>
2022-04-06 02:06:11 +0300

    add checkout component

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from "react";
+import Checkout from "../components/checkout/Checkout";
 import Header from "../components/Header";
 
 function CheckoutPage() {
   return (
     <Fragment>
       <Header />
-      <h1>Checkout</h1>
+      <Checkout/>
     </Fragment>
   );
 }
