fd7a2e0957b0926ba04bdd95ca121b7ef04de76d
WaruiAlfred <alfredwarui17@gmail.com>
2022-04-05 12:25:13 +0300

    pass down product details

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,70 @@
 import React from "react";
+import Product from "./Product";
 
 import classes from "./Home.module.css";
 
 function Home() {
   return (
     <div className={classes.home}>
-      <img className={classes['home__image']} src='https://m.media-amazon.com/images/I/61jovjd+f9L._SX3000_.jpg' alt="" />
+      <img
+        className={classes["home__image"]}
+        src="https://m.media-amazon.com/images/I/61jovjd+f9L._SX3000_.jpg"
+        alt=""
+      />
+
+      <div className={classes["home__row"]}>
+        <Product
+          id="1234"
+          title="The Legion"
+          price={11.23}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/913Fej9rOAL._AC_UY218_.jpg"
+        />
+
+        <Product
+          id="1134"
+          title="Toys"
+          price={15.13}
+          rating={4}
+          image="https://m.media-amazon.com/images/I/61LRy7J-eXL._AC_UL320_.jpg"
+        />
+      </div>
+
+      <div className={classes["home__row"]}>
+        <Product
+          id="1267"
+          title="Kitchen Ware"
+          price={9.23}
+          rating={3}
+          image="	https://m.media-amazon.com/images/I/71ygLu2o0OL._AC_SY200_.jpg"
+        />
+
+        <Product
+          id="1234"
+          title="Amazon bluetooth speaker"
+          price={21.23}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/61ASu-5HgBL._AC_SY200_.jpg"
+        />
+
+        <Product
+          id="1234"
+          title="E Reader"
+          price={9.12}
+          rating={3}
+          image="	https://m.media-amazon.com/images/I/61Ww4abGclL._AC_UY218_.jpg"
+        />
+      </div>
+
+      <div className={classes["home__row"]}>
+        <Product
+          id="1034"
+          title="Ski Mask"
+          price={14.23}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/51EdlHchUeL._AC_SY200_.jpg"
+        />
+      </div>
     </div>
   );
 }
